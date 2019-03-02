39ef34ba7603af06f4d6237f83e89a2207901774
Unknown <ken.will.king@gmail.com>
2019-03-02 17:36:01 -0500

    Redesigned survey panel looks.

--- src/wave/views/survey/SurveyPanel.java
+++ src/wave/views/survey/SurveyPanel.java
@@ -7,7 +7,6 @@ import javafx.beans.property.StringProperty;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
@@ -38,67 +37,67 @@ public class SurveyPanel extends BorderPane
 		this.isSurveyStarted = false;
 		this.questionTextProperty = new SimpleStringProperty();
 
+		// Top content
+		BorderPane titlePane = new BorderPane();
+		TextFlow titleText = new TextFlow();
+		titleText.setTextAlignment(TextAlignment.CENTER);
+		titleText.setPadding(new Insets(10, 10, 10, 10));
+		Text text1 = new Text();
+		text1.textProperty().bind(this.questionTextProperty);
+		this.questionTextProperty.setValue("Thank you for participating in the WAVE survey!");
+		titleText.getChildren().add(text1);
+		titlePane.setCenter(titleText);
+		titlePane.setBottom(new Separator());
+		this.setTop(titlePane);
+		
+		// Center content
+		TextFlow initialText = new TextFlow();
+		initialText.setTextAlignment(TextAlignment.CENTER);
+		initialText.setPadding(new Insets(10, 10, 10, 10));
+		Text text2 = new Text(
+				"In this experiment, you will be asked several questions related to the audio design of WAVE. ");
+		initialText.getChildren().add(text2);
+		Text text3 = new Text(
+				"The questions mainly consist of listening to various weather scenarios, and selecting an answer based upon the perceived audio. ");
+		initialText.getChildren().add(text3);
+		Text text4 = new Text("You must select an answer before proceeding to the next scenario. ");
+		initialText.getChildren().add(text4);
+		Text text5 = new Text(
+				"The survey will take approximately 15 minutes to complete, but you may leave at any time. \n\n");
+		initialText.getChildren().add(text5);
+		Text text6 = new Text("Click ");
+		initialText.getChildren().add(text6);
+		Text text7 = new Text("Start Survey ");
+		text7.setStyle("-fx-font-weight: bold");
+		initialText.getChildren().add(text7);
+		Text text8 = new Text("when you are ready to proceed. ");
+		initialText.getChildren().add(text8);
+		this.setCenter(initialText);
+		
+		// Bottom Content
 		this.buttonPanel = new GridPane();
 		this.buttonPanel.setPadding(new Insets(5, 5, 5, 5));
 		this.buttonPanel.setHgap(5);
 		this.buttonPanel.setVgap(5);
 		this.setBottom(this.buttonPanel);
 
-		ColumnConstraints questionColumn = new ColumnConstraints();
-		questionColumn.setHgrow(Priority.ALWAYS);
-		questionColumn.setHalignment(HPos.LEFT);
-		questionColumn.setPercentWidth(25);
-		this.buttonPanel.getColumnConstraints().add(questionColumn);
 		ColumnConstraints repeatColumn = new ColumnConstraints();
 		repeatColumn.setHgrow(Priority.ALWAYS);
-		repeatColumn.setHalignment(HPos.CENTER);
-		repeatColumn.setPercentWidth(50);
+		repeatColumn.setHalignment(HPos.LEFT);
 		this.buttonPanel.getColumnConstraints().add(repeatColumn);
 		ColumnConstraints nextColumn = new ColumnConstraints();
 		nextColumn.setHgrow(Priority.ALWAYS);
 		nextColumn.setHalignment(HPos.RIGHT);
-		nextColumn.setPercentWidth(25);
 		this.buttonPanel.getColumnConstraints().add(nextColumn);
 
-		// TODO disable until survey created
 		Separator separator = new Separator();
-		this.buttonPanel.add(separator, 0, 0, 3, 1);
-		Label questionLabel = new Label();
-		questionLabel.setTextAlignment(TextAlignment.CENTER);
-		questionLabel.minWidth(100);
-		questionLabel.textProperty().bind(this.questionTextProperty);
-		this.buttonPanel.add(questionLabel, 0, 1);
+		this.buttonPanel.add(separator, 0, 0, 2, 1);
 		this.nextScenario = new Button("Start Survey");
-		this.buttonPanel.add(this.nextScenario, 2, 1);
+		this.buttonPanel.add(this.nextScenario, 1, 1);
 		this.nextScenario.setOnAction((event) ->
 		{
 			nextScenario();
 		});
-
-		TextFlow initialText = new TextFlow();
-		initialText.setTextAlignment(TextAlignment.CENTER);
-		initialText.setPadding(new Insets(10, 10, 10, 10));
-		Text text1 = new Text("Thank you for participating in the WAVE survey! \n\n");
-		initialText.getChildren().addAll(text1);
-		Text text2 = new Text(
-				"In this experiment, you will be asked several questions related to the audio design of WAVE. ");
-		initialText.getChildren().addAll(text2);
-		Text text3 = new Text(
-				"The questions mainly consist of listening to various weather scenarios, and selecting an answer based upon the perceived audio. ");
-		initialText.getChildren().addAll(text3);
-		Text text4 = new Text("You must select an answer before proceeding to the next scenario. ");
-		initialText.getChildren().addAll(text4);
-		Text text5 = new Text(
-				"The survey will take approximately 15 minutes to complete, but you may leave at any time. \n\n");
-		initialText.getChildren().addAll(text5);
-		Text text6 = new Text("Click ");
-		initialText.getChildren().addAll(text6);
-		Text text7 = new Text("Start Survey ");
-		text7.setStyle("-fx-font-weight: bold");
-		initialText.getChildren().addAll(text7);
-		Text text8 = new Text("when you are ready to proceed. ");
-		initialText.getChildren().addAll(text8);
-		this.setCenter(initialText);
 	}
 
 	protected void startSurvey()
@@ -122,7 +121,7 @@ public class SurveyPanel extends BorderPane
 		{
 			startSurvey();
 			Button repeatSound = new Button("Repeat Sound");
-			this.buttonPanel.add(repeatSound, 1, 1);
+			this.buttonPanel.add(repeatSound, 0, 1);
 		}
 		SurveyScenario nextScenario = this.survey.getNextScenario();
 		QuestionPanel panel = QuestionPanel.CreatePanel(nextScenario);
