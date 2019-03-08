01eca181dec11b2472b4ffcca8e66961a91e9297
Unknown <ken.will.king@gmail.com>
2019-03-07 23:23:36 -0500

    Changed the setting initialization to use a method.

--- src/wave/WaveApp.java
+++ src/wave/WaveApp.java
@@ -5,13 +5,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 import gov.nasa.worldwind.Configuration;
-import gov.nasa.worldwind.WorldWind;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import wave.infrastructure.WaveSession;
 import wave.infrastructure.core.Wave;
+import wave.infrastructure.handlers.ErrorDialog;
 import wave.infrastructure.handlers.FXThemeLoader;
 import wave.infrastructure.preferences.PreferencesLoader;
 import wave.views.WaveWindow;
@@ -35,29 +35,35 @@ public class WaveApp extends Application
 	public void start(Stage primaryStage)
 	{
 		WaveApp.primaryStage = primaryStage;
-		WaveApp.session = new WaveSession();
-		WaveWindow window = new WaveWindow(session);
-		Scene primaryScene = new Scene(window, Wave.DEFAULT_WINDOW_WIDTH, Wave.DEFAULT_WINDOW_HEIGHT);
-		WaveApp.primaryStage.setTitle(Wave.APPLICATION_NAME);
-		WaveApp.primaryStage.setScene(primaryScene);
-		FXThemeLoader.applyDefaultTheme(primaryStage);
-		WaveApp.primaryStage.show();
+		if (initializeSettings())
+		{
+			WaveApp.session = new WaveSession();
+			WaveWindow window = new WaveWindow(session);
+			Scene primaryScene = new Scene(window, Wave.DEFAULT_WINDOW_WIDTH, Wave.DEFAULT_WINDOW_HEIGHT);
+			WaveApp.primaryStage.setTitle(Wave.APPLICATION_NAME);
+			WaveApp.primaryStage.setScene(primaryScene);
+			FXThemeLoader.applyDefaultTheme(primaryStage);
+			WaveApp.primaryStage.show();
+		}
 	}
 
 	@Override
 	public void stop()
 	{
-		WaveApp.session.shutdown();
+		if (WaveApp.session != null)
+		{
+			WaveApp.session.shutdown();
+		}
 		Platform.exit();
 	}
 
-	static
+	private boolean initializeSettings() 
 	{
 		System.setProperty("sun.awt.noerasebackground", "true");
 		Path configFile = Wave.WAVE_CONFIG_FILE;
 		Path wwConfigFile = Wave.WAVE_WW_CONFIG_FILE;
 
-		// TODO when error, throw javafx error dialog
+		boolean isInitialized = false;
 		try
 		{
 			PreferencesLoader.loadPreferences(configFile);
@@ -65,10 +71,14 @@ public class WaveApp extends Application
 			{
 				Configuration.insertConfigurationDocument(wwConfigFile.toAbsolutePath().toString());
 			}
+			isInitialized = true;
 		}
 		catch (IOException e)
 		{
-			e.printStackTrace();
+			StringBuilder errorMessage = new StringBuilder("Error: Unable to initialize settings. \n\n");
+			errorMessage.append(e.getMessage());
+			ErrorDialog.show(null, errorMessage.toString());
 		}
+		return isInitialized;
 	}
 }
