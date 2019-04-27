8fc5414a4209ffc664c85a3004dea593cf0065c2
Unknown <ken.will.king@gmail.com>
2019-04-26 22:54:57 -0400

    Changed marker textfields to spinners. Added modulated audio intensities for the various weather effects.

--- src/wave/audio/RainSounds.java
+++ src/wave/audio/RainSounds.java
@@ -21,11 +21,15 @@ public class RainSounds extends WeatherAudio
 	public static final Path HEAVY_FADE = Paths.get("data", "audio", "rain_heavy_fade.wav");
 
 	public static final File RAIN_FILE = new File("rain_sounds.wav");
+	public static final File RAIN_MOD_FILE = new File("rain_sounds_mod.wav");
 	
+	private Path modSoundPath;
 	private double intensity;
+	private double modGain;
 	private double scaleFactor;
 	private boolean isLoop;
-	private PlaySound sound;
+	private PlaySound baseSound;
+	private PlaySound modSound;
 	
 	public RainSounds(double intensity)
 	{
@@ -68,9 +72,13 @@ public class RainSounds extends WeatherAudio
 	public boolean stopAudio()
 	{
 		boolean hasStopped = false;
-		if (this.sound != null)
+		if (this.baseSound != null)
 		{
-			hasStopped = this.sound.stop();
+			hasStopped = this.baseSound.stop();
+		}
+		if (this.modSound != null)
+		{
+			this.modSound.stop();
 		}
 		return hasStopped;
 	}
@@ -81,18 +89,30 @@ public class RainSounds extends WeatherAudio
 		// @formatter:off
 		if (this.intensity > 0)
 		{
-			this.sound = new PlaySound(
+			this.baseSound = new PlaySound(
 					RAIN_FILE,
 					this.getHeadingIndex(), 
 					this.getElevationIndex(), 
 					this.soundPath, 
 					this.hrtf, 
-					this.scaleFactor, 
+					this.scaleFactor * (1 - this.modGain), 
+					this.duration,
+					this.isLoop);
+			this.modSound = new PlaySound(
+					RAIN_MOD_FILE,
+					this.getHeadingIndex(), 
+					this.getElevationIndex(), 
+					this.modSoundPath, 
+					this.hrtf, 
+					this.scaleFactor * this.modGain, 
 					this.duration,
 					this.isLoop);
 			// @formatter:on
-			Thread thread = new Thread(this.sound);
-			thread.start();
+			System.out.println(this.modGain);
+			Thread baseThread = new Thread(this.baseSound);
+			baseThread.start();
+			Thread modThread = new Thread(this.modSound);
+			modThread.start();
 		}
 	}
 
@@ -119,12 +139,18 @@ public class RainSounds extends WeatherAudio
 	public boolean fadeStop(long duration)
 	{
 		boolean hasStopped = false;
-		if (this.sound != null)
+		if (this.baseSound != null)
 		{
-			FadeEffect fade = new FadeEffect(this.sound, duration);
+			FadeEffect fade = new FadeEffect(this.baseSound, duration);
 			Thread thread = new Thread(fade);
 			thread.start();
 			hasStopped = true;
+		}
+		if (this.modSound != null)
+		{
+			FadeEffect modFade = new FadeEffect(this.modSound, duration);
+			Thread modThread = new Thread(modFade);
+			modThread.start();
 		}
 		return hasStopped;
 	}
@@ -132,31 +158,49 @@ public class RainSounds extends WeatherAudio
 	public void setIntensity(double intensity)
 	{
 		this.intensity = intensity;
+		this.modGain = 0;
 
 		if (intensity <= 0)
 		{
 			this.soundPath = null;
+			this.modSoundPath = null;
 		}
 		else if (intensity <= RainColorValues.RAIN_5.getMeasurement())
 		{
-			this.soundPath = RainSounds.LIGHTEST;
+			this.modSoundPath = RainSounds.LIGHTEST;
+			this.modGain = intensity;
+			this.modGain /= RainColorValues.RAIN_5.getMeasurement();
+			
 		}
-		else if (intensity <= RainColorValues.RAIN_15.getMeasurement())
+		else if (intensity <= RainColorValues.RAIN_20.getMeasurement())
+		{
+			this.soundPath = RainSounds.LIGHTEST;
+			this.modSoundPath = RainSounds.LIGHT;
+			this.modGain = intensity - RainColorValues.RAIN_5.getMeasurement();
+			this.modGain /= (RainColorValues.RAIN_20.getMeasurement() - RainColorValues.RAIN_5.getMeasurement());
+		}
+		else if (intensity <= RainColorValues.RAIN_32.getMeasurement())
 		{
 			this.soundPath = RainSounds.LIGHT;
+			this.modSoundPath = RainSounds.MEDIUM;
+			this.modGain = intensity - RainColorValues.RAIN_20.getMeasurement();
+			this.modGain /= (RainColorValues.RAIN_32.getMeasurement() - RainColorValues.RAIN_20.getMeasurement());
 		}
-		else if (intensity <= RainColorValues.RAIN_25.getMeasurement())
+		else if (intensity <= RainColorValues.RAIN_64.getMeasurement())
 		{
 			this.soundPath = RainSounds.MEDIUM;
-		}
-		else if (intensity <= RainColorValues.RAIN_38.getMeasurement())
-		{
-			this.soundPath = RainSounds.HEAVY;
+			this.modSoundPath = RainSounds.HEAVY;
+			this.modGain = intensity - RainColorValues.RAIN_32.getMeasurement();
+			this.modGain /= (RainColorValues.RAIN_64.getMeasurement() - RainColorValues.RAIN_32.getMeasurement());
 		}
 		else
 		{
-			this.soundPath = RainSounds.HEAVIEST;
+			this.soundPath = RainSounds.HEAVY;
+			this.modSoundPath = RainSounds.HEAVIEST;
+			this.modGain = intensity - RainColorValues.RAIN_127.getMeasurement();
+			this.modGain /= (RainColorValues.RAIN_127.getMeasurement() - RainColorValues.RAIN_64.getMeasurement());
 		}
+		this.modGain = Math.min(this.modGain, 1);
 	}
 	
 	public double getIntensity(double intensity)
--- src/wave/views/panels/MarkerPanel.java
+++ src/wave/views/panels/MarkerPanel.java
@@ -15,6 +15,8 @@ import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
@@ -35,35 +37,35 @@ import wave.infrastructure.preferences.Preferences;
 import wave.infrastructure.preferences.PreferencesLoader;
 
 public class MarkerPanel extends BorderPane implements ChangeListener<Object>
-{	
+{
 	private final WaveSession session;
 
 	private final Label latitudeLabel;
 	private final TextField latitudeTextfield;
 	private final Label latitudeUnitLabel;
 	private final Label longitudeLabel;
-	private final TextField longitudetextfield;
+	private final TextField longitudeTextfield;
 	private final Label longitudeUnitLabel;
 	private final Label elevationLabel;
 	private final TextField elevationTextfield;
 	private final Label elevationUnitLabel;
 	private final Label rainLabel;
-	private final TextField rainTextfield;
+	private final Spinner<Double> rainSpinner;
 	private final Label rainUnitLabel;
 	private final Label windSpeedLabel;
-	private final TextField windSpeedTextField;
+	private final Spinner<Double> windSpeedSpinner;
 	private final Label windSpeedUnitLabel;
 	private final Label windDirectionLabel;
-	private final TextField windDirectionTextField;
+	private final Spinner<Double> windDirectionSpinner;
 	private final Label windDirectionUnitLabel;
 	private final Label tempuratureLabel;
-	private final TextField temperatureTextfield;
+	private final Spinner<Double> temperatureSpinner;
 	private final Label temperatureUnitLabel;
 	private final Label humidityLabel;
-	private final TextField humidityTextfield;
+	private final Spinner<Double> humiditySpinner;
 	private final Label humidityUnitLabel;
 	private final Label lightningLabel;
-	private final TextField lightningTextfield;
+	private final Spinner<Double> lightningSpinner;
 	private final Label lightningUnitLabel;
 
 	private IconWeatherButton resetToggleButton;
@@ -71,7 +73,7 @@ public class MarkerPanel extends BorderPane implements ChangeListener<Object>
 	public MarkerPanel(WaveSession session)
 	{
 		this.session = session;
-		
+
 		MeasurementSystem system = PreferencesLoader.preferences().getLengthUnitDisplay();
 		ObjectProperty<Position> soundPosition = session.getSoundMarker().positionProperty();
 		HBox buttons = new HBox();
@@ -98,23 +100,25 @@ public class MarkerPanel extends BorderPane implements ChangeListener<Object>
 			Image annotationUnselectedImage = new Image(annotationUnselectedPath.toUri().toURL().toString());
 			Path annotationSelectedPath = Paths.get("data", "icons", "annotation_selected.png");
 			Image annotationSelectedImage = new Image(annotationSelectedPath.toUri().toURL().toString());
-			IconWeatherButton showAnnotationButton = new IconWeatherButton(annotationSelectedImage, annotationUnselectedImage);
+			IconWeatherButton showAnnotationButton = new IconWeatherButton(annotationSelectedImage,
+					annotationUnselectedImage);
 			showAnnotationButton.setTooltip(new Tooltip("Toggles whether the annotation will be displayed."));
 			showAnnotationButton.setIconSize(48, 48);
 			showAnnotationButton.setSelected(true);
 			showAnnotationButton.selectedProperty().bindBidirectional(pref.showAnnotationProperty());
 			buttons.getChildren().add(showAnnotationButton);
-						
+
 			Path resetUnselectedPath = Paths.get("data", "icons", "reset_unselected.png");
 			Image resetUnselectedImage = new Image(resetUnselectedPath.toUri().toURL().toString());
 			Path resetSelectedPath = Paths.get("data", "icons", "reset_selected.png");
 			Image resetSelectedImage = new Image(resetSelectedPath.toUri().toURL().toString());
 			this.resetToggleButton = new IconWeatherButton(resetSelectedImage, resetUnselectedImage);
-			this.resetToggleButton.setTooltip(new Tooltip("Toggles whether the data values update when the overlays are visible."));
+			this.resetToggleButton
+					.setTooltip(new Tooltip("Toggles whether the data values update when the overlays are visible."));
 			this.resetToggleButton.setIconSize(48, 48);
 			this.resetToggleButton.setSelected(true);
 			this.resetToggleButton.selectedProperty().bindBidirectional(pref.showAllWeatherProperty());
-			this.resetToggleButton.setOnAction(value -> 
+			this.resetToggleButton.setOnAction(value ->
 			{
 				session.updateWeatherAnnotation(session.getSoundMarker().getPosition());
 				session.getWorldWindow().redraw();
@@ -125,13 +129,15 @@ public class MarkerPanel extends BorderPane implements ChangeListener<Object>
 			Image positionUnselectedImage = new Image(positionUnselectedPath.toUri().toURL().toString());
 			Path positionSelectedPath = Paths.get("data", "icons", "position_selected.png");
 			Image positionSelectedImage = new Image(positionSelectedPath.toUri().toURL().toString());
-			IconWeatherButton showPositionButton = new IconWeatherButton(positionSelectedImage, positionUnselectedImage);
-			showPositionButton.setTooltip(new Tooltip("Toggles whether the position information will be added to the annotation."));
+			IconWeatherButton showPositionButton = new IconWeatherButton(positionSelectedImage,
+					positionUnselectedImage);
+			showPositionButton.setTooltip(
+					new Tooltip("Toggles whether the position information will be added to the annotation."));
 			showPositionButton.setIconSize(48, 48);
 			showPositionButton.setSelected(true);
 			showPositionButton.selectedProperty().bindBidirectional(pref.showPositionProperty());
 			showPositionButton.disableProperty().bind(showAnnotationButton.selectedProperty().not());
-			showPositionButton.setOnAction(value -> 
+			showPositionButton.setOnAction(value ->
 			{
 				session.updateWeatherAnnotation(session.getSoundMarker().getPosition());
 				session.getWorldWindow().redraw();
@@ -168,12 +174,12 @@ public class MarkerPanel extends BorderPane implements ChangeListener<Object>
 		this.latitudeLabel = new Label("Latitude:");
 		grid.add(this.latitudeLabel, 0, rowIndex);
 		this.latitudeTextfield = new TextField();
-		Bindings.bindBidirectional(this.latitudeTextfield.textProperty(), session.getWeatherHander().latitudeProperty(), new DoubleStringConverter());
+		Bindings.bindBidirectional(this.latitudeTextfield.textProperty(), session.getWeatherHander().latitudeProperty(),
+				new DoubleStringConverter());
 		grid.add(this.latitudeTextfield, 1, rowIndex);
 		this.latitudeUnitLabel = new Label(system.getAngleUnit());
 		grid.add(this.latitudeUnitLabel, 2, rowIndex);
 		rowIndex++;
-		// TODO add this stuff for the weather parameters
 		this.latitudeTextfield.focusedProperty().addListener(new ChangeListener<Boolean>()
 		{
 			@Override
@@ -210,13 +216,14 @@ public class MarkerPanel extends BorderPane implements ChangeListener<Object>
 
 		this.longitudeLabel = new Label("Longitude:");
 		grid.add(this.longitudeLabel, 0, rowIndex);
-		this.longitudetextfield = new TextField("No Data");
-		Bindings.bindBidirectional(this.longitudetextfield.textProperty(), session.getWeatherHander().longitudeProperty(), new DoubleStringConverter());
-		grid.add(this.longitudetextfield, 1, rowIndex);
+		this.longitudeTextfield = new TextField("No Data");
+		Bindings.bindBidirectional(this.longitudeTextfield.textProperty(),
+				session.getWeatherHander().longitudeProperty(), new DoubleStringConverter());
+		grid.add(this.longitudeTextfield, 1, rowIndex);
 		this.longitudeUnitLabel = new Label(system.getAngleUnit());
 		grid.add(this.longitudeUnitLabel, 2, rowIndex);
 		rowIndex++;
-		this.longitudetextfield.focusedProperty().addListener(new ChangeListener<Boolean>()
+		this.longitudeTextfield.focusedProperty().addListener(new ChangeListener<Boolean>()
 		{
 			@Override
 			public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue)
@@ -231,7 +238,7 @@ public class MarkerPanel extends BorderPane implements ChangeListener<Object>
 				});
 			}
 		});
-		this.longitudetextfield.setOnKeyPressed(new EventHandler<KeyEvent>()
+		this.longitudeTextfield.setOnKeyPressed(new EventHandler<KeyEvent>()
 		{
 
 			@Override
@@ -250,7 +257,8 @@ public class MarkerPanel extends BorderPane implements ChangeListener<Object>
 		this.elevationLabel = new Label("Elevation:");
 		grid.add(this.elevationLabel, 0, rowIndex);
 		this.elevationTextfield = new TextField("No Data");
-		Bindings.bindBidirectional(this.elevationTextfield.textProperty(), session.getWeatherHander().elevationProperty(), new DoubleStringConverter());
+		Bindings.bindBidirectional(this.elevationTextfield.textProperty(),
+				session.getWeatherHander().elevationProperty(), new DoubleStringConverter());
 		grid.add(this.elevationTextfield, 1, rowIndex);
 		this.elevationUnitLabel = new Label();
 		grid.add(this.elevationUnitLabel, 2, rowIndex);
@@ -288,54 +296,116 @@ public class MarkerPanel extends BorderPane implements ChangeListener<Object>
 
 		this.rainLabel = new Label("Rain:");
 		grid.add(this.rainLabel, 0, rowIndex);
-		this.rainTextfield = new TextField("No Data");
-		Bindings.bindBidirectional(this.rainTextfield.textProperty(), session.getWeatherHander().rainProperty(), new DoubleStringConverter());
-		grid.add(this.rainTextfield, 1, rowIndex);
+		this.rainSpinner = new Spinner<Double>();
+		this.rainSpinner.setEditable(true);
+		SpinnerValueFactory<Double> rainFactory = new SpinnerValueFactory.DoubleSpinnerValueFactory(0, 127, 0);
+		Bindings.bindBidirectional(session.getWeatherHander().rainProperty(), rainFactory.valueProperty());
+		this.rainSpinner.focusedProperty().addListener((observable, oldValue, newValue) ->
+		{
+			if (!newValue)
+			{
+				rainSpinner.increment(0);
+			}
+		});
+		this.rainSpinner.setValueFactory(rainFactory);
+		grid.add(this.rainSpinner, 1, rowIndex);
 		this.rainUnitLabel = new Label();
 		grid.add(this.rainUnitLabel, 2, rowIndex);
 		rowIndex++;
 
 		this.windSpeedLabel = new Label("Wind Spd:");
 		grid.add(this.windSpeedLabel, 0, rowIndex);
-		this.windSpeedTextField = new TextField("No Data");
-		Bindings.bindBidirectional(this.windSpeedTextField.textProperty(), session.getWeatherHander().windSpeedProperty(), new DoubleStringConverter());
-		grid.add(this.windSpeedTextField, 1, rowIndex);
+		this.windSpeedSpinner = new Spinner<Double>();
+		this.windSpeedSpinner.setEditable(true);
+		SpinnerValueFactory<Double> windSpeedFactory = new SpinnerValueFactory.DoubleSpinnerValueFactory(0, 127, 0);
+		Bindings.bindBidirectional(session.getWeatherHander().windSpeedProperty(), windSpeedFactory.valueProperty());
+		this.windSpeedSpinner.focusedProperty().addListener((observable, oldValue, newValue) ->
+		{
+			if (!newValue)
+			{
+				windSpeedSpinner.increment(0);
+			}
+		});
+		this.windSpeedSpinner.setValueFactory(windSpeedFactory);
+		grid.add(this.windSpeedSpinner, 1, rowIndex);
 		this.windSpeedUnitLabel = new Label();
 		grid.add(this.windSpeedUnitLabel, 2, rowIndex);
 		rowIndex++;
 
 		this.windDirectionLabel = new Label("Wind Dir:");
 		grid.add(this.windDirectionLabel, 0, rowIndex);
-		this.windDirectionTextField = new TextField("No Data");
-		Bindings.bindBidirectional(this.windDirectionTextField.textProperty(), session.getWeatherHander().windDirectionProperty(), new DoubleStringConverter());
-		grid.add(this.windDirectionTextField, 1, rowIndex);
+		this.windDirectionSpinner = new Spinner<Double>();
+		this.windDirectionSpinner.setEditable(true);
+		SpinnerValueFactory<Double> windDirectionFactory = new SpinnerValueFactory.DoubleSpinnerValueFactory(0, 127, 0);
+		Bindings.bindBidirectional(session.getWeatherHander().windDirectionProperty(),
+				windDirectionFactory.valueProperty());
+		this.windDirectionSpinner.focusedProperty().addListener((observable, oldValue, newValue) ->
+		{
+			if (!newValue)
+			{
+				windDirectionSpinner.increment(0);
+			}
+		});
+		this.windDirectionSpinner.setValueFactory(windDirectionFactory);
+		grid.add(this.windDirectionSpinner, 1, rowIndex);
 		this.windDirectionUnitLabel = new Label(system.getAngleUnit());
 		grid.add(this.windDirectionUnitLabel, 2, rowIndex);
 		rowIndex++;
 
 		this.tempuratureLabel = new Label("Temp.:");
 		grid.add(this.tempuratureLabel, 0, rowIndex);
-		this.temperatureTextfield = new TextField("No Data");
-		Bindings.bindBidirectional(this.temperatureTextfield.textProperty(), session.getWeatherHander().temperatureProperty(), new DoubleStringConverter());
-		grid.add(this.temperatureTextfield, 1, rowIndex);
+		this.temperatureSpinner = new Spinner<Double>();
+		this.temperatureSpinner.setEditable(true);
+		SpinnerValueFactory<Double> temperatureFactory = new SpinnerValueFactory.DoubleSpinnerValueFactory(0, 127, 0);
+		Bindings.bindBidirectional(session.getWeatherHander().temperatureProperty(),
+				temperatureFactory.valueProperty());
+		this.temperatureSpinner.focusedProperty().addListener((observable, oldValue, newValue) ->
+		{
+			if (!newValue)
+			{
+				temperatureSpinner.increment(0);
+			}
+		});
+		this.temperatureSpinner.setValueFactory(temperatureFactory);
+		grid.add(this.temperatureSpinner, 1, rowIndex);
 		this.temperatureUnitLabel = new Label();
 		grid.add(this.temperatureUnitLabel, 2, rowIndex);
 		rowIndex++;
 
 		this.humidityLabel = new Label("Humidity:");
 		grid.add(this.humidityLabel, 0, rowIndex);
-		this.humidityTextfield = new TextField("No Data");
-		Bindings.bindBidirectional(this.humidityTextfield.textProperty(), session.getWeatherHander().humidityProperty(), new DoubleStringConverter());
-		grid.add(this.humidityTextfield, 1, rowIndex);
+		this.humiditySpinner = new Spinner<Double>();
+		this.humiditySpinner.setEditable(true);
+		SpinnerValueFactory<Double> humidityFactory = new SpinnerValueFactory.DoubleSpinnerValueFactory(0, 127, 0);
+		Bindings.bindBidirectional(session.getWeatherHander().humidityProperty(), humidityFactory.valueProperty());
+		this.humiditySpinner.focusedProperty().addListener((observable, oldValue, newValue) ->
+		{
+			if (!newValue)
+			{
+				humiditySpinner.increment(0);
+			}
+		});
+		this.humiditySpinner.setValueFactory(humidityFactory);
+		grid.add(this.humiditySpinner, 1, rowIndex);
 		this.humidityUnitLabel = new Label();
 		grid.add(this.humidityUnitLabel, 2, rowIndex);
 		rowIndex++;
 
 		this.lightningLabel = new Label("Lightning:");
 		grid.add(this.lightningLabel, 0, rowIndex);
-		this.lightningTextfield = new TextField("No Data");
-		Bindings.bindBidirectional(this.lightningTextfield.textProperty(), session.getWeatherHander().lightningProperty(), new DoubleStringConverter());
-		grid.add(this.lightningTextfield, 1, rowIndex);
+		this.lightningSpinner = new Spinner<Double>();
+		this.lightningSpinner.setEditable(true);
+		SpinnerValueFactory<Double> lightningFactory = new SpinnerValueFactory.DoubleSpinnerValueFactory(0, 127, 0);
+		Bindings.bindBidirectional(session.getWeatherHander().lightningProperty(), lightningFactory.valueProperty());
+		this.lightningSpinner.focusedProperty().addListener((observable, oldValue, newValue) ->
+		{
+			if (!newValue)
+			{
+				lightningSpinner.increment(0);
+			}
+		});
+		this.lightningSpinner.setValueFactory(lightningFactory);
+		grid.add(this.lightningSpinner, 1, rowIndex);
 		this.lightningUnitLabel = new Label("km^2/yr");
 		grid.add(this.lightningUnitLabel, 2, rowIndex);
 		rowIndex++;
@@ -348,7 +418,7 @@ public class MarkerPanel extends BorderPane implements ChangeListener<Object>
 		});
 		grid.add(updateButton, 1, rowIndex, 2, 1);
 		rowIndex++;
-		
+
 		updateUnitLabels();
 		updateMarkerValues(soundPosition.get());
 		this.setCenter(grid);
@@ -364,7 +434,7 @@ public class MarkerPanel extends BorderPane implements ChangeListener<Object>
 				});
 			}
 		});
-		
+
 		Button playSoundButton = new Button("Sound");
 		playSoundButton.setMaxWidth(Double.MAX_VALUE);
 		playSoundButton.setOnAction((event) ->
@@ -375,7 +445,7 @@ public class MarkerPanel extends BorderPane implements ChangeListener<Object>
 		});
 		grid.add(playSoundButton, 1, rowIndex, 2, 1);
 		rowIndex++;
-		
+
 		PreferencesLoader.preferences().lengthUnitDisplayProperty().addListener(this);
 	}
 
@@ -398,7 +468,7 @@ public class MarkerPanel extends BorderPane implements ChangeListener<Object>
 	{
 		double latitude = this.session.getSoundMarker().getPosition().latitude.degrees;
 		double elevation = this.session.getSoundMarker().getPosition().elevation;
-		double longitude = Double.valueOf(this.longitudetextfield.getText());
+		double longitude = Double.valueOf(this.longitudeTextfield.getText());
 		Position newPosition = Position.fromDegrees(latitude, longitude, elevation);
 		this.session.getSoundMarker().setPosition(newPosition);
 		this.session.getWorldWindow().redraw();
